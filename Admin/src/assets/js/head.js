9badc764aae04c0d94e70b84fb14c455f78cdb41
Curtis Henley <curtisdhenley@gmail.com>
2023-07-08 15:05:52 -0400

    creating bio page

---
+++ Admin/src/assets/js/head.js
@@ -0,0 +1,12 @@
+/*
+Template Name: Ubold - Responsive Bootstrap 5 Admin Dashboard
+Author: CoderThemes
+Website: https://coderthemes.com/
+Contact: support@coderthemes.com
+File: Main Head Js File
+*/
+
+//  RTL support js
+if (document.getElementById('app-style').href.includes('rtl.min.css')) {
+    document.getElementsByTagName('html')[0].dir = "rtl";
+}
